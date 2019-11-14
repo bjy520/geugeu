ce52c276e4c457777d5a1fbd0d3bcc8fa9578351
bjy520 <735036965@qq.com>
2019-11-14 16:02:20 +0800

    First commit.

---
+++ app/src/main/java/com/shuaiyu/xuexi/Main3Activity.java
@@ -0,0 +1,13 @@
+package com.shuaiyu.xuexi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
